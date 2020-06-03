3a82b2a4acfd847c9fc0324752380fd554726083
jinnic <yoohyojin@gmail.com>
2020-06-03 15:22:09 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,57 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  # Read
   get '/' do
+    redirect to "/articles"
+  end
+
+  # Read
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  # Create
+  get '/articles/new' do
+    # @article = Article.new
+    erb :new
+  end
+
+  # Create
+  post '/articles' do 
+    # binding.pry
+    @article = Article.create(title: params[:title],content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+  
+  # Read
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  # Update
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  # patch '/articles/:id' do 
+  #   article = Article.update(params[:article])
+  #   redirect '/article/#{article.id}'
+  # end
+
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title],content: params[:content])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  # Delete
+  delete '/articles/:id' do 
+    Article.destroy(params[:id])
+    redirect '/articles'
+    # erb :index
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,13 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    # attr_accessor :title, :content
+    # @@all = []
+
+    # def initialize(argument)
+    #     @argument = argument
+    # end
+    # def self.all
+    #     @@all
+    # end
 end
